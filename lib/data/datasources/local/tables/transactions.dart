af0e9f7dd09ba9bb59bf97adb0a132ef6e932562
Kiran Sai <39314292+jalakamkiran@users.noreply.github.com>
2025-07-15 20:12:08 +0530

    main : modify : removed unwanted dependencies

--- lib/data/datasources/local/tables/transactions.dart
+++ lib/data/datasources/local/tables/transactions.dart
@@ -7,7 +7,7 @@ class Transactions extends Table {
   TextColumn get category => text()();
   TextColumn get wallet => text()();
   TextColumn get description => text().nullable()();
-  TextColumn get type => text()(); // Changed from IntColumn to TextColumn
+  TextColumn get type => text()();
   DateTimeColumn get date => dateTime()();
   TextColumn get attachment => text().nullable()();
   BoolColumn get repeat => boolean().withDefault(Constant(false))();
--- lib/presentation/screens/dashboard/dashboard_screen.dart
+++ lib/presentation/screens/dashboard/dashboard_screen.dart
@@ -1,19 +1,11 @@
 import 'package:expense_tracker_clean/application/blocs/bottom_nav_bar/bottom_nav_bloc.dart';
 import 'package:expense_tracker_clean/application/blocs/bottom_nav_bar/bottom_nav_event.dart';
-import 'package:expense_tracker_clean/application/blocs/home/home_bloc.dart';
-import 'package:expense_tracker_clean/application/blocs/home/home_state.dart';
 import 'package:expense_tracker_clean/core/res/app_colors.dart';
-import 'package:expense_tracker_clean/core/res/app_text_styles.dart';
-import 'package:expense_tracker_clean/core/res/res.dart';
 import 'package:expense_tracker_clean/presentation/screens/dashboard/modules/home/home_screen.dart';
 import 'package:expense_tracker_clean/presentation/screens/dashboard/modules/transactions/transactions_screen.dart';
 import 'package:expense_tracker_clean/presentation/screens/dashboard/widgets/custom_fab_bar.dart';
-import 'package:expense_tracker_clean/presentation/screens/dashboard/modules/home/widgets/month_dropdown.dart';
-import 'package:expense_tracker_clean/presentation/screens/dashboard/modules/home/widgets/transaction_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'widgets/custom_bottom_navigation_bar.dart';
 
